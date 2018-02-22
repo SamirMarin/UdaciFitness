c0804d2e17a8643bfa9fd51214cb888d2ac080cc
Samir Marin <marin.samir@gmail.com>
2018-02-21 18:10:57 -0800

    Make and style workout history view

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
+import History from './components/History'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
@@ -14,7 +15,8 @@ export default class App extends React.Component {
     return (
       <Provider store={this.store}>
         <View style={{flex: 1 }}>
-          <AddEntry />
+          <View style={{height: 20}}/>
+          <History />
         </View>
       </Provider>
     );
