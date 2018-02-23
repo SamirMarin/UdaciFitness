bb586aa14219908934c677c2ba16ed87ec37b542
Samir Marin <marin.samir@gmail.com>
2018-02-22 20:53:53 -0800

    Create live component Track location, altitude and direction

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
 import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
+import { setLocalNotification } from './utils/helpers'
 
 function UdaciStatusBar ({ backgroundColor, ...props }) {
   return (
@@ -33,8 +35,14 @@ const Tabs = TabNavigator({
       tabBarLabel: 'Add Entry',
       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
     }
-
-  }
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: 'Live',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={30} color={tintColor}/>
+    }
+  },
 }, {
   navigationOptions: {
     header: null,
@@ -72,6 +80,10 @@ const MainNavigator = StackNavigator({
 
 export default class App extends React.Component {
   store = createStore(reducer)
+
+  componentDidMount() {
+    setLocalNotification()
+  }
 
   render() {
     return (
